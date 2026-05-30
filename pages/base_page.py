16cb44e4b6522137157ae557e314debbefb84a13
Serhii Uhalnykov <serhii.uhalnykov@gmail.com>
2026-05-30 15:03:03 +0300

    refactor: change BasePage to be a superclass for target site pages

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,12 +2,23 @@ import allure
 from playwright.sync_api import Page
 
 from utils.config import URL
+from utils.logger import get_logger
+
+logger = get_logger(__name__)
 
 class BasePage:
-    
+    BASE_URL: str = URL
+    PATH: str = ""
+
     def __init__(self, page: Page) -> None:
-        self.page: Page = page
+        logger.info(f"Initializaing {self.__class__.__name__}")
+        self._page: Page = page
+        self._url: str = self.BASE_URL + self.PATH
+
+    @property
+    def url(self) -> str:
+        return self._page.url
 
     @allure.step("Open page")
     def open(self) -> None:
-        self.page.goto(URL)
+        self._page.goto(self._url)
